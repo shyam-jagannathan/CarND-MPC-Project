fb2b2cd591dae2625d690c0995050880206688f1
shyam-jagannathan <shyam.jagannathan@gmail.com>
2017-06-23 07:46:00 +0530

    Add files via upload
    Modified code for adjusting for latency of 100ms

--- src/main.cpp
+++ src/main.cpp
@@ -106,10 +106,10 @@ int main() {
 
           //predict state in 100ms
           double latency = 0.1;
-          //px = px + v * cos(psi) * latency;
-          //py = py + v * sin(psi) * latency;
-          //psi = psi + v * delta/Lf * latency;
-          //v = v + acc * latency;
+          px = px + v * cos(psi) * latency;
+          py = py + v * sin(psi) * latency;
+          psi = psi - v * delta/Lf * latency;
+          v = v + acc * latency;
 
           Eigen::VectorXd ptsx_v(ptsx.size());
           Eigen::VectorXd ptsy_v(ptsy.size());
@@ -180,7 +180,7 @@ int main() {
           //
           // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
           // SUBMITTING.
-          this_thread::sleep_for(chrono::milliseconds(0));
+          this_thread::sleep_for(chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
